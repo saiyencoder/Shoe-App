8e2af07b3e73840db97eb49a1b0c5830c55775d7
saiyencoder <jsantizo20@gmail.com>
2017-09-11 21:32:42 -0500

    class exercise implemented

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -2,6 +2,31 @@ class ShoesController < ApplicationController
 
   def index
     @shoes = Shoe.all
+
+    sort_attribute = params[:sort]
+    desc_sort_attribute = params[:desc]
+    discount_items = params[:discount]
+    random_shoe = params[:random]
+
+    if sort_attribute
+      @shoes = @shoes.order(sort_attribute)
+    end
+
+    if sort_attribute && desc_sort_attribute
+      @shoes = @shoes.order(sort_attribute).reverse
+    end
+
+    if sort_attribute && discount_items
+      @shoes = Shoe.where("price < ?", 120)
+    end
+
+    # if sort_attribute && random_shoe
+    #   random_shoe_id = rand(1..Shoe.count)
+    #   @shoes = Shoe.find(random_shoe_id)
+    #   redirect_to '/shoes/#{ @shoes }'
+    # end
+
+
   end
 
   def new
